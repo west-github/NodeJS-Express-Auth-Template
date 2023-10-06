2db59c18c1ac7ecfd86b651abdb5a45da69b2548
west-github <westcoder@outlook.com>
2023-10-06 13:10:51 +0100

    Add and Update ReadME

---
+++ README.md
@@ -0,0 +1,7 @@
+# Node Js Express Authentication Template
+
+```javascript
+pnpm/npm -i  
+```
+
+Free for everyone grabbing a copy lol! 😁😁😁
--- middleware/protected.ts
+++ middleware/protected.ts
@@ -2,6 +2,8 @@ import { NextFunction as NF, Request, Response } from "express";
 
 // This middleware will only check the header to see if we have authentication credentials else let us go
 // This middleware won't parse the authentication credentials that's the job of the global middleware
+// TODO: This middleware in future will add a query value of true if user has authentication credentials this is useful for user to let them know
+// what they are trying to do
 export const protected_mw = (req: Request, res: Response, next: NF) => {
     if (req.headers._auth) {
         // they have authentication credentials here we will redirect them to dashboard or homepage
